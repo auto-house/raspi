793c514ff34ba9c843c5bf2c4a3002a3702ec104
Vítor Barros <vba2@cin.ufpe.br>
2016-11-21 20:14:09 -0300

    Fix e Add menu
    Arrumar chamada da função de ativação do módulo relé.
    Adicionar menu para facilitar testes e futuramente chamada das funções.

--- main.py
+++ main.py
@@ -10,24 +10,29 @@ GPIO.setmode(GPIO.BOARD)
 #define the pin that goes to the circuit
 pino_sensor_interno = 7
 pino_sensor_externo = 11
-pino_rele = 12
-GPIO.setup(12, GPIO.OUT)
+pino_rele = 13
+GPIO.setup(13, GPIO.OUT)
 
 cond_loop = True
+menu = 0
 
 #Catch when script is interrupted, cleanup correctly
 try:
     # Main loop
     while cond_loop:
+
+    	menu = int(raw_input("1- Sensor Interno\n2- Sensor Externo\n3- Ligar Rele\n4- Desligar Rele\nQual opcao: "))
         
-        print ("Nivel Sensor Interno: " + sensoresLuz.rc_time(pino_sensor_interno))
-        print ("Nivel Sensor Externo " + sensoresLuz.rc_time(pino_sensor_externo))
+    	if menu == 1:
+			print ("Nivel Sensor Interno: " + str(sensoresLuz.rc_time(pino_sensor_interno)))
+    	elif menu == 2:
+			print ("Nivel Sensor Externo " + str(sensoresLuz.rc_time(pino_sensor_externo)))
+    	elif menu == 3:
+			moduloRele.ativar(pino_rele)
+    	elif menu == 4:
+        	moduloRele.desativar(pino_rele)
+    	else:
+    		cond_loop = False
 
-        moduloRele.ativer(pino_rele)
-        time.sleep(2)
-        moduloRele.desativar(pino_rele)
-
-except KeyboardInterrupt:
-    pass
 finally:
     GPIO.cleanup()
